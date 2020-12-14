e6f350c63df9ae2dca27c7fcd637d9d805489eb4
Agustin Ojeda <agustinojeda20@gmail.com>
2020-12-13 21:58:53 -0300

    vueTutorial

---
+++ vue/01.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        titulo: "Hola mundo con Vue",
+        frutas: [
+            {nombre:'Pera', cantidad:10},
+            {nombre:'Manzana', cantidad:0},
+            {nombre:'Banana', cantidad:11},
+        ],
+        nuevaFruta:"",
+    },
+    methods:{
+        agregarFruta () {
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad:0
+            });
+            this.nuevaFruta = "";
+        },
+        otroMetodo (){
+
+        },
+
+    }
+})
